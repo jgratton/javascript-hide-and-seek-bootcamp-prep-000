91e15dd7046eada4e5af83a65d76129e171c21f3
John Gratton <jgratton@gmail.com>
2018-03-27 00:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget(nested, target) {
-  return document.querySelector(`${nested} ${target}`);
+function nestedTarget(nestedTarget) {
+  return document.querySelector(nestedTarget);
 }
